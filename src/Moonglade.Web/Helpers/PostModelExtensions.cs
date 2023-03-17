944bc86d790cce7681c3aea09e6c8d0cd88e3675
saigkill <Sascha.Manns@outlook.de>
2023-03-17 23:17:17 +0000

    sync dev from git to azure

--- src/Moonglade.Web/Helpers/PostModelExtensions.cs
+++ src/Moonglade.Web/Helpers/PostModelExtensions.cs
@@ -11,14 +11,14 @@ namespace Moonglade.Web.Helpers
         /// </summary>
         /// <param name="story">The story.</param>
         /// <returns>TimeToRead object with calculated reading time.</returns>
-        public static TimeToRead GetTimeToRead(this Post story, IStringLocalizer<SharedResource> localizer)
+        public static TimeToRead GetTimeToRead(this Post story)
         {
             string wordCount = story.RawPostContent; // Get all content of the story
             int counts = wordCount.Count(ch => ch == ' ') + 1;
             int minutes = counts / 200; // Calculate Minutes
             int seconds = counts % 200 / (200 / 60); // Calculate Seconds
-            string strMinutes = (minutes == 1) ? localizer["Minute"] : localizer["Minutes"];
-            string strSeconds = (seconds == 1) ? localizer["Second"] : localizer["Seconds"];
+            string strMinutes = (minutes == 1) ? "Minute" : "Minutes";
+            string strSeconds = (seconds == 1) ? "Second" : "Seconds";
             TimeToRead timeToRead = new()
             {
                 Minutes = minutes,
