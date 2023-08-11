cbd6302cdac13531876b0c2c3bb11d85e7bd2a52
Sascha Manns <Sascha.Manns@outlook.de>
2023-08-11 20:10:59 +0200

    Last updates

--- src/Moonglade.Data.DataProviders/Models/Talk.cs
+++ src/Moonglade.Data.DataProviders/Models/Talk.cs
@@ -2,84 +2,83 @@
 using System.ComponentModel.DataAnnotations;
 
 using Moonglade.Utils;
-using System.ComponentModel.DataAnnotations;
 
 namespace Moonglade.Data.DataProviders.Models
 {
-    /// <summary>
-    /// Talks model.
-    /// </summary>
-    public class Talk
-    {
-        /// <summary>
-        /// Gets or sets the identifier.
-        /// </summary>
-        /// <value>
-        /// The identifier.
-        /// </value>
-        [Required]
-        public int Id { get; set; }
+	/// <summary>
+	/// Talks model.
+	/// </summary>
+	public class Talk
+	{
+		/// <summary>
+		/// Gets or sets the identifier.
+		/// </summary>
+		/// <value>
+		/// The identifier.
+		/// </value>
+		[Required]
+		public int Id { get; set; }
 
-        /// <summary>
-        /// Gets or sets the title.
-        /// </summary>
-        /// <value>
-        /// The title.
-        /// </value>
-        [Required]
-        public string Title { get; set; }
+		/// <summary>
+		/// Gets or sets the title.
+		/// </summary>
+		/// <value>
+		/// The title.
+		/// </value>
+		[Required]
+		public string Title { get; set; }
 
-        /// <summary>
-        /// Gets or sets the where.
-        /// </summary>
-        /// <value>
-        /// The where.
-        /// </value>
-        [Required]
-        public string Where { get; set; }
+		/// <summary>
+		/// Gets or sets the where.
+		/// </summary>
+		/// <value>
+		/// The where.
+		/// </value>
+		[Required]
+		public string Where { get; set; }
 
-        /// <summary>
-        /// Gets or sets the link.
-        /// </summary>
-        /// <value>
-        /// The link.
-        /// </value>
-        public string Link { get; set; }
+		/// <summary>
+		/// Gets or sets the link.
+		/// </summary>
+		/// <value>
+		/// The link.
+		/// </value>
+		public string Link { get; set; }
 
-        /// <summary>
-        /// Gets or sets the blurp.
-        /// </summary>
-        /// <value>
-        /// The blurp.
-        /// </value>
-        [Required]
-        public string Blurp { get; set; }
+		/// <summary>
+		/// Gets or sets the blurp.
+		/// </summary>
+		/// <value>
+		/// The blurp.
+		/// </value>
+		[Required]
+		public string Blurp { get; set; }
 
-        /// <summary>
-        /// Gets or sets the date.
-        /// </summary>
-        /// <value>
-        /// The date.
-        /// </value>
-        [Required]
-        public DateTime Date { get; set; }
+		/// <summary>
+		/// Gets or sets the date.
+		/// </summary>
+		/// <value>
+		/// The date.
+		/// </value>
+		[Required]
+		public DateTime Date { get; set; }
 
-        /// <summary>
-        /// Gets or sets the type of the talk.
-        /// </summary>
-        /// <value>
-        /// The type of the talk.
-        /// </value>
-        [Required]
-        public TalkType TalkType { get; set; }
+		/// <summary>
+		/// Gets or sets the type of the talk.
+		/// </summary>
+		/// <value>
+		/// The type of the talk.
+		/// </value>
+		[Required]
+		public TalkType TalkType { get; set; }
 
-        /// <summary>
-        /// Gets or sets the language.
-        /// </summary>
-        /// <value>
-        /// The language.
-        /// </value>
-        [Required]
-        public LanguageEnum Language { get; set; }
-    }
+		/// <summary>
+		/// Gets or sets the language.
+		/// </summary>
+		/// <value>
+		/// The language.
+		/// </value>
+		[Required]
+		public LanguageEnum Language { get; set; }
+	}
 }
--- src/Moonglade.Data.DataProviders/Models/Testimonial.cs
+++ src/Moonglade.Data.DataProviders/Models/Testimonial.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.DataAnnotations;
 
 using Moonglade.Utils;
-using System.ComponentModel.DataAnnotations;
 
 namespace Moonglade.Data.DataProviders.Models;
 
@@ -11,90 +10,90 @@ namespace Moonglade.Data.DataProviders.Models;
 /// </summary>
 public class Testimonial
 {
-    /// <summary>
-    /// Gets or sets the identifier.
-    /// </summary>
-    /// <value>
-    /// The identifier.
-    /// </value>
-    [Required]
-    public int Id { get; set; }
+	/// <summary>
+	/// Gets or sets the identifier.
+	/// </summary>
+	/// <value>
+	/// The identifier.
+	/// </value>
+	[Required]
+	public int Id { get; set; }
 
-    /// <summary>
-    /// Gets or sets the link.
-    /// </summary>
-    /// <value>
-    /// The link.
-    /// </value>
-    public string Link { get; set; }
+	/// <summary>
+	/// Gets or sets the link.
+	/// </summary>
+	/// <value>
+	/// The link.
+	/// </value>
+	public string Link { get; set; }
 
-    /// <summary>
-    /// Gets or sets the blurp.
-    /// </summary>
-    /// <value>
-    /// The blurp.
-    /// </value>
-    [Required]
-    public string Blurp { get; set; }
+	/// <summary>
+	/// Gets or sets the blurp.
+	/// </summary>
+	/// <value>
+	/// The blurp.
+	/// </value>
+	[Required]
+	public string Blurp { get; set; }
 
-    /// <summary>
-    /// Gets or sets the recommender.
-    /// </summary>
-    /// <value>
-    /// The recommender.
-    /// </value>
-    [Required]
-    public string Recommender { get; set; }
+	/// <summary>
+	/// Gets or sets the recommender.
+	/// </summary>
+	/// <value>
+	/// The recommender.
+	/// </value>
+	[Required]
+	public string Recommender { get; set; }
 
-    /// <summary>
-    /// Gets or sets the recommender job.
-    /// </summary>
-    /// <value>
-    /// The recommender job.
-    /// </value>
-    public string RecommenderJob { get; set; }
+	/// <summary>
+	/// Gets or sets the recommender job.
+	/// </summary>
+	/// <value>
+	/// The recommender job.
+	/// </value>
+	public string RecommenderJob { get; set; }
 
-    /// <summary>
-    /// Gets or sets the recommender location.
-    /// </summary>
-    /// <value>
-    /// The recommender location.
-    /// </value>
-    public string RecommenderLocation { get; set; }
+	/// <summary>
+	/// Gets or sets the recommender location.
+	/// </summary>
+	/// <value>
+	/// The recommender location.
+	/// </value>
+	public string RecommenderLocation { get; set; }
 
-    /// <summary>
-    /// Gets or sets the relationship.
-    /// </summary>
-    /// <value>
-    /// The relationship.
-    /// </value>
-    [Required]
-    public string Relationship { get; set; }
+	/// <summary>
+	/// Gets or sets the relationship.
+	/// </summary>
+	/// <value>
+	/// The relationship.
+	/// </value>
+	[Required]
+	public string Relationship { get; set; }
 
-    /// <summary>
-    /// Gets or sets the image path.
-    /// </summary>
-    /// <value>
-    /// The image path.
-    /// </value>
-    [Required]
-    public string ImagePath { get; set; }
+	/// <summary>
+	/// Gets or sets the image path.
+	/// </summary>
+	/// <value>
+	/// The image path.
+	/// </value>
+	[Required]
+	public string ImagePath { get; set; }
 
-    /// <summary>
-    /// Gets or sets the date.
-    /// </summary>
-    /// <value>
-    /// The date.
-    /// </value>
-    [Required]
-    public DateTime Date { get; set; }
+	/// <summary>
+	/// Gets or sets the date.
+	/// </summary>
+	/// <value>
+	/// The date.
+	/// </value>
+	[Required]
+	public DateTime Date { get; set; }
 
-    /// <summary>
-    /// Gets or sets the language.
-    /// </summary>
-    /// <value>
-    /// The language.
-    /// </value>
-    [Required]
-    public LanguageEnum Language { get; set; }
+	/// <summary>
+	/// Gets or sets the language.
+	/// </summary>
+	/// <value>
+	/// The language.
+	/// </value>
+	[Required]
+	public LanguageEnum Language { get; set; }
 }
