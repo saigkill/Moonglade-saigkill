c151f050fbdb14dab5ca4b4b8c82c307542b8b53
Sascha Manns <Sascha.Manns@outlook.de>
2023-12-11 11:44:30 +0100

    Small layout fixes in Settings. Updated commit.sh for use in Azure

--- src/commit.sh
+++ src/commit.sh
@@ -8,20 +8,39 @@ GHPAT=$GHPAT
 git config --global user.email "$AZUSER_EMAIL"
 git config --global user.name "$AZUSERNAME"
 
-git clone https://$GHUSER:$GHPAT@github.com/saigkill/Moonglade ./Moonglade-gh
+git clone https://$GHUSER:$GHPAT@github.com/$GHUSER/Moonglade ./Moonglade-gh
 GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/Moonglade/_git/Moonglade"
-git clone $GIT_CMD_REPOSITORY ./Moonglade-az
+git clone $GIT_CMD_REPOSITORY ./Moonglade-az-develop
+git clone $GIT_CMD_REPOSITORY ./Moonglade-az-master
 
-cd Moonglade-az
+echo "Checking out develop"
+pushd Moonglade-az-develop
+git checkout develop
 rm -rf .git
-cd ..
+popd
 
-cp -r Moonglade-az/* Moonglade-gh/
+echo "Checking out master"
+pushd Moonglade-az-master
+git checkout master
+rm -rf .git
+popd
+
+echo "Copying new stuff from Azure dev to Github"
+cp -r Moonglade-az-develop/* Moonglade-gh/
+
+echo "Checking out master and add new stuff"
+pushd Moonglade-gh
+git checkout develop
+git add .
+git commit -m "sync dev from azure to github"
+git push
+popd
 
 pushd Moonglade-gh
-
+git checkout master
+echo "Copying new stuff from Azure dev to Github"
+cp -r Moonglade-az-master/* .
 git add .
-git commit -m "sync prod from azure to github"
+git commit -m "sync master from azure to github"
 git push
-
 popd
