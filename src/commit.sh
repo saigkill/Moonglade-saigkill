18530d6a0c8b9566b676e13f3ffab583ecf20091
Sascha Manns <Sascha.Manns@outlook.de>
2023-12-11 14:24:11 +0100

    Fixed error in commit.sh

--- src/commit.sh
+++ src/commit.sh
@@ -1,4 +1,3 @@
-AZUREPAT=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
@@ -8,16 +7,10 @@ GHPAT=$GHPAT
 git config --global user.email "$AZUSER_EMAIL"
 git config --global user.name "$AZUSERNAME"
 
-git clone https://$GHUSER:$GHPAT@github.com/$GHUSER/Moonglade ./Moonglade-gh
+git clone https://$GHUSER:$GHPAT@github.com/$GHUSER/Moonglade-$GHUSER ./Moonglade-gh
 GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/Moonglade/_git/Moonglade"
 git clone $GIT_CMD_REPOSITORY ./Moonglade-az-develop
 git clone $GIT_CMD_REPOSITORY ./Moonglade-az-master
-
-echo "Checking out develop"
-pushd Moonglade-az-develop
-git checkout develop
-rm -rf .git
-popd
 
 echo "Checking out master"
 pushd Moonglade-az-master
@@ -25,21 +18,25 @@ git checkout master
 rm -rf .git
 popd
 
-echo "Copying new stuff from Azure dev to Github"
-cp -r Moonglade-az-develop/* Moonglade-gh/
-
-echo "Checking out master and add new stuff"
-pushd Moonglade-gh
+echo "Checking out develop"
+pushd Moonglade-az-develop
 git checkout develop
-git add .
-git commit -m "sync dev from azure to github"
-git push
+rm -rf .git
 popd
 
-pushd Moonglade-gh
-git checkout master
 echo "Copying new stuff from Azure dev to Github"
-cp -r Moonglade-az-master/* .
+cp -r Moonglade-az-master/* Moonglade-gh/
+
+pushd Moonglade-gh
+git checkout main
+echo "Copying new stuff from Azure master to Github"
+git add .
+git commit -m "sync master from azure to github"
+git push
+
+git checkout develop
+cp -r ../Moonglade-az-develop/* .
+echo "Copying new stuff from Azure dev to Github"
 git add .
 git commit -m "sync master from azure to github"
 git push
