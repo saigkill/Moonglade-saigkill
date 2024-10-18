3f99b825666110445b5f2c6432fe985f7386499a
Sascha Manns <S.Manns@infas.de>
2024-10-18 19:53:01 +0200

    feat: Now it shows only posts, who matching the langCode

--- src/Moonglade.Data/Specifications/PostPagingSpec.cs
+++ src/Moonglade.Data/Specifications/PostPagingSpec.cs
@@ -1,46 +1,51 @@
-﻿using Moonglade.Data.Entities;
+﻿using System.Globalization;
+
+using Moonglade.Data.Entities;
 
 namespace Moonglade.Data.Specifications;
 
 public sealed class PostPagingSpec : Specification<PostEntity>
 {
-    public PostPagingSpec(int pageSize, int pageIndex, Guid? categoryId = null)
-    {
-        Query.Where(p => !p.IsDeleted && p.IsPublished &&
+  public PostPagingSpec(int pageSize, int pageIndex, Guid? categoryId = null)
+  {
+    var langCode = CultureInfo.CurrentUICulture.ToString().ToLower();
+    Query.Where(p => !p.IsDeleted && p.IsPublished &&
                          (categoryId == null || p.PostCategory.Select(c => c.CategoryId).Contains(categoryId.Value)));
 
-        var startRow = (pageIndex - 1) * pageSize;
+    var startRow = (pageIndex - 1) * pageSize;
 
-        Query.OrderByDescending(p => p.PubDateUtc);
-        Query.Skip(startRow).Take(pageSize);
-    }
+    Query.Where(p => p.ContentLanguageCode == LanguageExtensions.CheckLanguage(langCode));
+
+    Query.OrderByDescending(p => p.PubDateUtc);
+    Query.Skip(startRow).Take(pageSize);
+  }
 }
 
 public sealed class PostPagingByStatusSpec : Specification<PostEntity>
 {
-    public PostPagingByStatusSpec(PostStatus postStatus, string keyword, int pageSize, int offset)
+  public PostPagingByStatusSpec(PostStatus postStatus, string keyword, int pageSize, int offset)
+  {
+    Query.Where(p => null == keyword || p.Title.Contains(keyword));
+
+    switch (postStatus)
     {
-        Query.Where(p => null == keyword || p.Title.Contains(keyword));
-
-        switch (postStatus)
-        {
-            case PostStatus.Draft:
-                Query.Where(p => !p.IsPublished && !p.IsDeleted);
-                break;
-            case PostStatus.Published:
-                Query.Where(p => p.IsPublished && !p.IsDeleted);
-                break;
-            case PostStatus.Deleted:
-                Query.Where(p => p.IsDeleted);
-                break;
-            case PostStatus.Default:
-                Query.Where(p => true);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(postStatus), postStatus, null);
-        }
-
-        Query.Skip(offset).Take(pageSize);
-        Query.OrderByDescending(p => p.PubDateUtc);
+      case PostStatus.Draft:
+        Query.Where(p => !p.IsPublished && !p.IsDeleted);
+        break;
+      case PostStatus.Published:
+        Query.Where(p => p.IsPublished && !p.IsDeleted);
+        break;
+      case PostStatus.Deleted:
+        Query.Where(p => p.IsDeleted);
+        break;
+      case PostStatus.Default:
+        Query.Where(p => true);
+        break;
+      default:
+        throw new ArgumentOutOfRangeException(nameof(postStatus), postStatus, null);
     }
+
+    Query.Skip(offset).Take(pageSize);
+    Query.OrderByDescending(p => p.PubDateUtc);
+  }
 }
